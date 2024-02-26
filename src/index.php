0cc1b87d14af58a400a260b0e06d8887a0d4bc03
web1337alex <alex1337webdev@gmail.com>
2024-02-26 15:39:15 +0500

    Enable Nginx and PHP

--- src/index.php
+++ src/index.php
@@ -1,39 +1,15 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>Hello there</title>
-    <style>
-        body {
-            font-family: "Arial", sans-serif;
-            font-size: larger;
-        }
-
-        .center {
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-            width: 50%;
-        }
-    </style>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
 <body>
-<img src="https://tech.osteel.me/images/2020/03/04/hello.gif" alt="Hello there" class="center">
-<?php
-$connection = new PDO('mysql:host=mysql;dbname=demo;charset=utf8', 'root', 'root');
-$query      = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'demo'");
-$tables     = $query->fetchAll(PDO::FETCH_COLUMN);
-
-if (empty($tables)) {
-    echo '<p class="center">There are no tables in database <code>demo</code>.</p>';
-} else {
-    echo '<p class="center">Database <code>demo</code> contains the following tables:</p>';
-    echo '<ul class="center">';
-    foreach ($tables as $table) {
-        echo "<li>{$table}</li>";
-    }
-    echo '</ul>';
-}
-?>
+    <?php
+        echo "Вставить текст";
+    ?>
 </body>
 </html>
